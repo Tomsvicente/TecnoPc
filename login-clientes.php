d59728e3727e6b441996a8a681f7715bb80d9fc0
Tomas <tomsebvicente@gmail.com>
2019-11-22 00:55:42 -0300

    commit on master

--- login-clientes.php
+++ login-clientes.php
@@ -16,7 +16,7 @@
         <input type="text" class="form-control" required>
         <label for="">Ingrese su contraseña</label>
         <input type="password" class="form-control" required>
-         <input type="submit" value="Entrar" class="btn btn-success">
+        <input type="submit" value="Entrar" class="btn btn-success">
     </form>
 
 </div>
@@ -24,4 +24,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
-</html> 
+</html>
