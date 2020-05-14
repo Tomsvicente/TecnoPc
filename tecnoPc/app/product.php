ae6d9a7150c45ec3c74d496050e1936dcde75e08
darian <la12_dari@hotmail.com>
2020-05-13 22:13:26 -0300

    creacion-migraciones

---
+++ tecnoPc/app/product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class product extends Model
+{
+     //crear una migracion en git nota:  php artisan make:migration create_directos_table
+    //
+}
